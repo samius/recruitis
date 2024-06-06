2efa759a9c047a9d6833bd963e60d354b9275713
Milan Krupa <samius05@gmail.com>
2024-06-07 01:29:55 +0200

    webpack, bootstrap, guzzle added

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+// assets/app.js
+
+import 'bootstrap';
+import './styles/app.scss';
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+// import './bootstrap';
