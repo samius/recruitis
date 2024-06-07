7992a5cdce84e274b5f397ffe2a7e9ccd775992a
Milan Krupa <samius05@gmail.com>
2024-06-07 23:11:07 +0200

    Caching api responses for 60s

--- src/Integrations/Recruitis/Client.php
+++ src/Integrations/Recruitis/Client.php
@@ -6,13 +6,16 @@ namespace App\Integrations\Recruitis;
 use App\Integrations\Recruitis\DataObject\Jobs\JobCollection;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\RequestOptions;
+use Psr\Cache\CacheItemInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class Client
 {
     public static string $BASE_URL = 'https://app.recruitis.io/api2/';
+    private static int $CACHE_EXPIRE = 60;
 
-    public function __construct(private readonly string $apiToken, private readonly ClientInterface $httpClient, private readonly SerializerInterface $serializer)
+    public function __construct(private readonly string $apiToken, private readonly ClientInterface $httpClient, private readonly SerializerInterface $serializer, private readonly CacheInterface $cache)
     {
     }
 
@@ -25,7 +28,19 @@ class Client
 
     private function get(string $path, array $query): string
     {
-        return $this->sendRequest('GET', $path, [RequestOptions::QUERY => $query]);
+        return $this->getCachedResult('GET', $path, [RequestOptions::QUERY => $query]);
+    }
+
+    private function getCachedResult(string $method, string $path, array $config): string
+    {
+        $cacheKey = $this->getCacheKey($method, $path, $config);
+
+        return $this->cache->get (
+            $cacheKey, function (CacheItemInterface $cacheItem) use ($method, $path, $config) {
+                $cacheItem->expiresAfter(self::$CACHE_EXPIRE);
+                return $this->sendRequest($method, $path, $config);
+            }
+        );
     }
 
     private function sendRequest(string $method, string $path, array $config): string
@@ -34,6 +49,12 @@ class Client
 
         $response = $this->httpClient->request($method, self::$BASE_URL . $path, $config);
         return $response->getBody()->getContents();
+    }
+
+
+    private function getCacheKey(string $method, string $path, array $config): string
+    {
+        return sha1($method . $path . serialize($config));
     }
 
     private function getRequestHeaders(): array
