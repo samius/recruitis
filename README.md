1bb30bcc8c7b99194117c26affab3cb796bee2e4
Milan Krupa <samius05@gmail.com>
2024-06-10 02:44:51 +0200

    templates and css

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # recruitis
+
+Job id 413548 má nekonzistentní salary - isRange=true, ale isMaxVisible=false
+Education je jeden objekt, podle dokumentace by mělo být pole
--- src/Integrations/Recruitis/Model/Jobs/Job/Salary.php
+++ src/Integrations/Recruitis/Model/Jobs/Job/Salary.php
@@ -2,11 +2,19 @@
 declare(strict_types=1);
 
 namespace App\Integrations\Recruitis\Model\Jobs\Job;
+use Symfony\Component\Serializer\Attribute\SerializedName;
 
 class Salary
 {
     private ?float $min = null;
+
+    #[SerializedName('is_min_visible')]
+    private bool $minVisible = false;
     private ?float $max = null;
+    #[SerializedName('is_max_visible')]
+    private bool $maxVisible = false;
+    #[SerializedName('is_range')]
+    private bool $isRange = false;
     private ?string $currency = null;
     private ?string $unit = null;
     private ?bool $visible = null;
@@ -30,6 +38,39 @@ class Salary
     public function setMax(float $max): Salary
     {
         $this->max = $max;
+        return $this;
+    }
+
+    public function isMinVisible(): bool
+    {
+        return $this->minVisible;
+    }
+
+    public function setMinVisible(bool $minVisible): Salary
+    {
+        $this->minVisible = $minVisible;
+        return $this;
+    }
+
+    public function isMaxVisible(): bool
+    {
+        return $this->maxVisible;
+    }
+
+    public function setMaxVisible(bool $maxVisible): Salary
+    {
+        $this->maxVisible = $maxVisible;
+        return $this;
+    }
+
+    public function isRange(): bool
+    {
+        return $this->isRange;
+    }
+
+    public function setIsRange(bool $isRange): Salary
+    {
+        $this->isRange = $isRange;
         return $this;
     }
 
